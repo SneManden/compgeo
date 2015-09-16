2430c4476dabced5cc52b4fec9972ff579445614
Casper Kehlet Jensen <casper@snemanden.com>
2015-09-16 21:57:53 +0200

    Now using stdbool

--- lib/util.h
+++ lib/util.h
@@ -1,12 +1,11 @@
 #ifndef __UTIL_H
 #define __UTIL_H
 
+#include <stdbool.h>
+
 #define DO_PRAGMA(x) _Pragma (#x)
 #define TODO(x) DO_PRAGMA(message ("TODO - " #x))
 // Usage:
 // TODO(Remember to fix this)
-
-#define true 1
-#define false 0
 
 #endif /* __UTIL_H */
